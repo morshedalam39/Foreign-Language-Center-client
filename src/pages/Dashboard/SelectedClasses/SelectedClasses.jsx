8bf3e9821f7789e00cc1a32f4ca11e8faf931d55
morshedalam39 <morshedalam4239@gmail.com>
2023-06-10 13:09:55 +0600

    selected class data load

--- src/pages/Dashboard/SelectedClasses/SelectedClasses.jsx
+++ src/pages/Dashboard/SelectedClasses/SelectedClasses.jsx
@@ -5,9 +5,53 @@ const SelectedClasses = () => {
     const {data}=useSelectedClass()
     console.log(data);
     return (
-        <div>
-            selectedClasses
-        </div>
+        <div className='w-10/12 mx-auto'>
+        <div className="overflow-x-auto">
+ <table className="table ">
+   <thead>
+       
+     <tr>
+       <th>#</th> 
+       <th>Picture</th> 
+
+       <th>Class Name</th> 
+
+       <th>Price</th> 
+       <th>Action</th> 
+       <th>Payment</th> 
+
+     </tr>
+   </thead> 
+   <tbody>
+   {data?.map((cls, index) => 
+               <tr key={cls._id}>
+               <th>{index +1}</th> 
+               <td>
+               <div className="avatar">
+                     <div className="mask mask-squircle w-12 h-12">
+                       <img src={cls.classImage} alt="Avatar Tailwind CSS Component" />
+                     </div>
+                   </div>
+                   </td> 
+            
+               <td>{cls.className}</td> 
+ 
+               <td>${cls.price}</td> 
+               <td><button className="btn btn-error btn-sm">Delete</button></td>
+               <td><button className="btn btn-success btn-sm">Pay</button></td>
+
+       
+             </tr>
+           )}
+
+
+     
+
+   </tbody> 
+
+ </table>
+</div>
+       </div>
     );
 };
 
